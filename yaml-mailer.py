51043ca21c688a3e79c94fe0f4b50e856c9e4347
Clifford Ilkay <cilkay@gmail.com>
2018-05-30 22:22:30 -0400

    Added config keys for message and attachments paths.
    Made script PEP8 compliant.

--- yaml-mailer.py
+++ yaml-mailer.py
@@ -10,74 +10,109 @@ from email.encoders import encode_base64
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
-from os.path import basename,expanduser
+from os import path
 import smtplib
 import sys
 import yaml
 
-def die(msg, code=42): print('Error: ' + msg); sys.exit(code);
+
+def die(msg, code=42):
+    print('Error: ' + msg)
+    sys.exit(code)
+
+
 def check_key(tag, dict_obj, key):
-  if key not in dict_obj:
-    die(tag + " does not contain '" + key + "' key.")
+    if key not in dict_obj:
+        die(tag + " does not contain '" + key + "' key.")
+
 
 parser = argparse.ArgumentParser(description='Mail people with YAML.')
 parser.add_argument('messages_yaml', type=str)
 parser.add_argument('--config', type=str,
-    default=expanduser("~")+"/.mailer.yaml")
+                    default=path.expanduser("~")+"/.mailer.yaml")
 parser.add_argument('--dry-run', action='store_true')
 args = parser.parse_args()
 
-with open(args.messages_yaml, 'r') as f: yaml_contents = yaml.load(f)
-with open(args.config, 'r') as f: yaml_config = yaml.load(f)
+# Configuration file
+with open(args.config, 'r') as f:
+    yaml_config = yaml.load(f)
 
 check_key("Yaml config", yaml_config, 'smtp')
 smtp = yaml_config['smtp']
+
 for key in ['server', 'port', 'tls', 'user', 'password']:
-  check_key("Yaml config - smtp", smtp, key)
+    check_key("Yaml config - smtp", smtp, key)
 
 server = smtplib.SMTP(smtp['server'], smtp['port'])
 server.ehlo_or_helo_if_needed()
-if smtp['tls']: server.starttls()
+
+if smtp['tls']:
+    server.starttls()
 server.login(smtp['user'], smtp['password'])
+
+check_key("Yaml config", yaml_config, 'paths')
+paths = yaml_config['paths']
+
+for key in ['messages_path', 'attachments_path']:
+    check_key("Yaml config - paths", paths, key)
+
+# Message file
+message_file = path.join(paths['messages_path'], args.messages_yaml)
+
+with open(message_file, 'r') as f:
+    yaml_contents = yaml.load(f)
+
 
 check_key("Yaml file", yaml_contents, 'messages')
 messages = yaml_contents['messages']
+
 for msg in messages:
-  for key in ['to', 'subject', 'contents']:
-    check_key('Message with contents: ' + str(msg), msg, key)
+    for key in ['to', 'subject', 'contents']:
+        check_key('Message with contents: ' + str(msg), msg, key)
 
-  # Make non-list fields lists.
-  for field in ['to', 'cc', 'bcc', 'attach']:
-    if field in msg and not isinstance(msg[field], list):
-      msg[field] = [msg[field]]
+    # Make non-list fields lists.
+    for field in ['to', 'cc', 'bcc', 'attach']:
+        if field in msg and not isinstance(msg[field], list):
+            msg[field] = [msg[field]]
 
-  multi_msg = MIMEMultipart()
-  multi_msg['From'] = smtp['from']
-  multi_msg['To'] = ",".join(msg['to'])
-  multi_msg['Subject'] = msg['subject']
-  if 'cc' in msg: multi_msg['CC'] = ",".join(msg['cc'])
-  multi_msg.attach(MIMEText(msg['contents']))
+    multi_msg = MIMEMultipart()
+    multi_msg['From'] = smtp['from']
+    multi_msg['To'] = ",".join(msg['to'])
+    multi_msg['Subject'] = msg['subject']
+    if 'cc' in msg:
+        multi_msg['CC'] = ",".join(msg['cc'])
+    multi_msg.attach(MIMEText(msg['contents']))
 
-  if 'attach' in msg:
-    for f_name in msg['attach']:
-      part = MIMEBase('application', "octet-stream")
-      with open(f_name, 'rb') as f: part.set_payload(f.read())
-      encode_base64(part)
-      part.add_header('Content-Disposition',
-          'attachment; filename="' + basename(f_name) + '"')
-      multi_msg.attach(part)
+    if 'attach' in msg:
+        for f_name in msg['attach']:
+            part = MIMEBase('application', "octet-stream")
+            attachment_file = path.join(paths['attachments_path'], f_name)
+            with open(attachment_file, 'rb') as f:
+                part.set_payload(f.read())
+            encode_base64(part)
+            part.add_header('Content-Disposition',
+                            'attachment; filename="' + path.basename(f_name) + '"')
+            multi_msg.attach(part)
 
-  print("Sending message.")
-  print("  To: " + ",".join(msg['to']))
-  if 'cc' in msg: print("  CC: " + ",".join(msg['cc']))
-  if 'bcc' in msg: print("  BCC: " + ",".join(msg['bcc']))
-  print("  Subject: " + msg['subject'])
-  if 'attach' in msg: print("  Attach: " + ",".join(msg['attach']))
-  if not args.dry_run:
-    server.sendmail(smtp['from'], msg['to'], multi_msg.as_string())
+    print("Sending message.")
+    print("    To: " + ",".join(msg['to']))
+
+    if 'cc' in msg:
+        print("    CC: " + ",".join(msg['cc']))
+
     if 'bcc' in msg:
-      for bcc in msg['bcc']:
-        server.sendmail(smtp['from'], bcc, multi_msg.as_string())
+        print("    BCC: " + ",".join(msg['bcc']))
+
+    print("    Subject: " + msg['subject'])
+
+    if 'attach' in msg:
+        print("    Attach: " + ",".join(msg['attach']))
+
+    if not args.dry_run:
+        server.sendmail(smtp['from'], msg['to'], multi_msg.as_string())
+        if 'bcc' in msg:
+            for bcc in msg['bcc']:
+                server.sendmail(smtp['from'], bcc, multi_msg.as_string())
 
 print("All messages sent successfully.")
 server.close()
